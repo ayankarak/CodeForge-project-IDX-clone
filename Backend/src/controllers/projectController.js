f1268f6f11099baec22d0ed83064637d6d3f0fdb
Ayan <ayankarak2005@gmail.com>
2026-09-19 00:23:10 +0530

    folder struvcture backend using directory tree is added

--- Backend/src/controllers/projectController.js
+++ Backend/src/controllers/projectController.js
@@ -1,18 +1,16 @@
-import utils from 'util';
-import child_process from 'child_process';
-import fs from 'fs/promises';
-import uuid4 from 'uuid4';
-
-const execPromisified = utils.promisify(child_process.exec);
+import { createProjectService, getProjectTreeService } from "../service/projectService.js";
 
 export const createProjectController = async (req, res) => {
-    const projectId = uuid4();
-    console.log("new project id is: ", projectId);
-    await fs.mkdir(`./projects/${projectId}`);
-    // after this the npm create vite command in the newly created directory and then return a success message to the client.
-    const response= await execPromisified('npm create vite@latest sandbox -- --template react', { 
-        cwd: `./projects/${projectId}`
-    });
+    const projectId = await createProjectService
     
     return res.json({ message: 'Project created successfully' });
+}
+
+export const getProjectTree=async(req, res)=>{
+    const tree=await getProjectTreeService(req.params.projectId);
+    return res.status(200).json({
+        data: tree,
+        success:true,
+        message:"Successfully fetched the tree"
+    })
 }
