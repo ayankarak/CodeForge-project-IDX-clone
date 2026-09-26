423e6cfc23acda4b411cc06bdf81ee6e90d611f2
Ayan <ayankarak2005@gmail.com>
2026-09-27 00:15:31 +0530

    added docker config

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,6 +9,7 @@ import chokidar from 'chokidar';
 //import path from 'node:path';
 import { handleEditorSocketEvents } from './socketHandlers/editorHandler.js';
 import queryString from 'query-string'
+import { handleContainerCreate } from './containers/handleContainerCreate.js';
 
 const app = express();
 const server = createServer(app);
@@ -67,6 +68,23 @@ editorNamespace.on("connection", (socket) => {
     // });
 
 });
+
+const terminalNamespace = io.of('/terminal');
+terminalNamespace.on("connection", (socket) => {
+    console.log("terminal connected");
+
+    let projectId = socket.handshake.query['projectId'];
+
+    socket.on("shell-input", (data) => {
+        console.log("input recevied", data);
+        terminalNamespace.emit("shell-output", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("terminal disconnected");
+    });
+    handleContainerCreate(projectId, socket);
+})
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
